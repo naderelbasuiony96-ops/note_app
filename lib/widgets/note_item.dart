5e7562b056b5c48376370869bc7b72247e2ebd62
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-04-19 10:57:26 +0200

    we create new folder (fonts) inside assets and add Poppins font

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -11,12 +11,23 @@ class NoteItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           ListTile(
-            title: Text('Flutter Tips  '),
-            subtitle: Text(' build new project with Nader Amr '),
-            trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            subtitle: Text(
+              ' build new project with Nader Amr ',
+              style: TextStyle(color: Colors.black12, fontSize: 16),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.black),
+            ),
           ),
+          Text('May21, 2026', style: TextStyle(color: Colors.black)),
         ],
       ),
     );
