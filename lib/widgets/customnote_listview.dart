07793a21dd63c1875745ccdcc7d59cbf36f7a37a
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-04-19 13:57:30 +0200

    create listview to show dynamic item  for ui

---
+++ lib/widgets/customnote_listview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
