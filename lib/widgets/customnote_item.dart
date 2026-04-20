3ee9a27e04e745a49887000e8a97e6576dcb0628
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-04-20 13:54:22 +0200

    create edit note view

--- lib/widgets/customnote_item.dart
+++ lib/widgets/customnote_item.dart
@@ -1,52 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit%20_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: const Text(
-                'Build project with Nader Amr ',
-                style: TextStyle(
-                  color: Color.fromARGB(85, 0, 0, 0),
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: const Text(
+                  'Build project with Nader Amr ',
+                  style: TextStyle(
+                    color: Color.fromARGB(85, 0, 0, 0),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: FaIcon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 25,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: FaIcon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 25,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21, 2026',
+                style: TextStyle(color: Colors.black.withValues(alpha: 0.4)),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21, 2026',
-              style: TextStyle(color: Colors.black.withValues(alpha: 0.4)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
