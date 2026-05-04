ab7dafd485482fc2c56d080e149722c60287ac0a
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-05 01:52:30 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -4,16 +4,3 @@ part of 'notes_cubit.dart';
 sealed class NotesState {}
 
 final class NotesInitial extends NotesState {}
-
-final class NotesLoading extends NotesState {}
-
-final class NotesSuccess extends NotesState {
-  final List<NoteModel> note;
-
-  NotesSuccess(this.note);
-}
-
-final class NotesFailuer extends NotesState {
-  final String errMessage;
-  NotesFailuer(this.errMessage);
-}
