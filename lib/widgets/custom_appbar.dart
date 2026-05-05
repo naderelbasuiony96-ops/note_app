c0393a799b3128ea0a9dea10be6e080bed2024b1
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-05 17:55:32 +0300

    details of edit note done

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/customsearch_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.text, required this.icon});
+  const CustomAppBar(
+      {super.key, required this.text, required this.icon, this.onPressed});
   final String text;
   final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Text(text, style: const TextStyle(color: Colors.white, fontSize: 28)),
         Spacer(),
-        CustomSearchIcon(icon: icon),
+        CustomSearchIcon(onPressed: onPressed, icon: icon),
       ],
     );
   }
--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -17,7 +17,9 @@ class NoteItem extends StatelessWidget {
           context,
           MaterialPageRoute(
             builder: (context) {
-              return const EditNoteView();
+              return EditNoteView(
+                note: note,
+              );
             },
           ),
         );
