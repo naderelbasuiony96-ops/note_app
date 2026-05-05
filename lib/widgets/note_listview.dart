9da18f53accb0e752ddc5342c24ad707ded23665
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-05 16:12:24 +0300

    display data part 2

--- lib/widgets/note_listview.dart
+++ lib/widgets/note_listview.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/widgets/customnote_item.dart';
+import 'package:notes_app/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
@@ -18,9 +18,11 @@ class NotesListView extends StatelessWidget {
             itemCount: notes.length,
             padding: EdgeInsets.zero,
             itemBuilder: (context, index) {
-              return const Padding(
+              return Padding(
                 padding: EdgeInsets.symmetric(vertical: 4),
-                child: NoteItem(),
+                child: NoteItem(
+                  note: notes[index],
+                ),
               );
             },
           ),
